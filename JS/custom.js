fe797f6f1e58a847dc14240cf60ad5acbc4461f3
Dan <16627962@students.lincoln.ac.uk>
2018-01-07 20:06:45 +0000

    Added shirt customiser

--- JS/custom.js
+++ JS/custom.js
@@ -1,2 +1,17 @@
+window.onload = function() {
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(50,50,350,400);
+	ctx.drawImage(img,300-(a/2),300-(a/2));
+};
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+function ColorUpdate() {
+	var n = document.getElementsByName("favcolor")[0].value;
+	console.log(n);
+	ctx.fillStyle = n;
+	ctx.fillRect(50,50,350,400);
+	ctx.drawImage(img,300-(a/2),300-(a/2));
+}
+var a = 500;
+var img = new Image(a, a);
+img.src = 'Assets/Untitled.png';
